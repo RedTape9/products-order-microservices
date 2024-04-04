44fec24b4fe443dccd4f24de3911d86097948b6e
Sergej Jaudszims <redtape9688@gmail.com>
2024-04-04 11:11:27 +0200

    add webClient call to send GET request to inventory service

--- order-service/src/main/java/io/github/redtape9/orderservice/service/OrderService.java
+++ order-service/src/main/java/io/github/redtape9/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import io.github.redtape9.orderservice.model.OrderLineItems;
 import io.github.redtape9.orderservice.repo.OrderRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,6 +20,7 @@ public class OrderService {
 
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -30,8 +32,17 @@ public class OrderService {
         order.setOrderLineItemsList(orderLineItems);
 
         // call inventoryservice to check if the items are available and place order
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+        if(result){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not availible in inventory.");
+        }
 
-        orderRepository.save(order);
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
